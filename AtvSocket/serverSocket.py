0c3124633f97f8389908316566a35263879d36b0
rabbit11 <pedro.coelho@dcomp.sor.ufscar.br>
2019-04-04 12:50:21 -0300

    correção socket server

--- AtvSocket/serverSocket.py
+++ AtvSocket/serverSocket.py
@@ -3,15 +3,15 @@ import socket
 
 
 TCP_IP = '127.0.0.1'
-TCP_PORT = 3000
-BUFFER_SIZE = 5120
+TCP_PORT = 8080
+BUFFER_SIZE = 1024
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
 
 conn, addr = s.accept()
-print 'Connection address:', addr
+print ('Connection address:', addr)
 while 1:
     data = conn.recv(BUFFER_SIZE)
     if not data: break
